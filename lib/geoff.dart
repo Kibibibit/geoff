01004c45797b6bcae1aff4c29ca2fbbde4b17f42
Daniel Mills <danielmills@DESKTOP-D9V2TKA.localdomain>
2022-02-10 14:53:45 +1100

    Created models and widgets sub libs

--- lib/geoff.dart
+++ lib/geoff.dart
@@ -1,7 +1,4 @@
 library geoff;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export "models.dart";
+export "widgets.dart";
---
+++ lib/models/models.dart
@@ -0,0 +1,1 @@
+library geoff.models
