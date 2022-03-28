549a04e9e046d90599a5cf174e00dc2ff9425850
Dan M <daniel@thewindmills.com.au>
2022-03-28 15:02:50 +1100

    Added wrap functions

--- lib/utils/maths/number_utils.dart
+++ lib/utils/maths/number_utils.dart
@@ -18,5 +18,30 @@ class NumberUtils {
   }
 
 
+  /// Wraps a double [val] between [min] and [max]. If [min] = 1, [max] = 5
+  /// and [val] = 7, this will return 2.
+  static double wrapDouble(double val, double min, double max) {
+
+    double diff = (max-min).abs() + 1;
+
+    while (val < min) {
+      val += diff;
+    }
+
+    while (val > max) {
+      val -= diff;
+    }
+
+    return val;
+
+  }
+
+  /// Wraps an integer [val] between [min] and [max]. If [min] = 1, [max] = 5
+  /// and [val] = 7, this will return 2.
+  static int wrapInt(int val, int min, int max) {
+    return wrapDouble(val.toDouble(), min.toDouble(), max.toDouble()).toInt();
+  }
+
+
 
 }
